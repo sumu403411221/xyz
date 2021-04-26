bd85013f169e8b3beeb9c56d3481031a49aaaf16
SUMIT KUMAR THAKUR <sumitskt39@gmail.com>
2021-04-26 14:29:24 +0000

    hvd

---
+++ app.js
@@ -0,0 +1,30 @@
+var express     =require("express");
+var app         =express();
+var bodyParser  =require("body-parser");
+var request = require("request");
+
+app.set("view engine","ejs");
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+app.get("/",function(req,res){
+	var url = "(https://api.wazirx.com/api/v2/tickers";
+	request(url,function(error,response,body){
+		if(!error && response.statusCode==200){
+			var data = JSON.parse(body);
+			console.log(data);
+			res.render("index.ejs",{data:data});
+			
+		}
+	})
+	
+	
+	
+	
+})
+
+
+
+app.listen(process.env.PORT || 3000,function(){
+	console.log("server started");
+})
